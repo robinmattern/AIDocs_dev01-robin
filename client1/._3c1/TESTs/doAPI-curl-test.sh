7894a9a001ca7f593431f9f4d305ad94b858480b
Robin Mattern <robin.mattern@gmail.com>
2024-04-05 11:01:12 -0400

    c40402.05_Add doAPI-*.sh to ./client1/._3/TESTs

---
+++ client1/._3c1/TESTs/doAPI-curl-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+  aHost="http://155.138.193.41:3001"
+  aWorkspace="visitor1-workspace"
+  aAPI="api/v1/workspace/${aWorkspace}/chat"
+  aANYLLM_API_KEY="6Q5P8YR-JXAMFGB-KGGEZ6T-W94PXE3"
+  aPrompt="What is AnythingLLM?"
+  aMode="query"
+
+#curl -X 'POST' \
+#  'http://155.138.193.41:3001/api/v1/workspace/visitor1-workspace/chat' \
+#  -H 'accept: application/json' \
+#  -H 'Authorization: Bearer 6Q5P8YR-JXAMFGB-KGGEZ6T-W94PXE3' \
+#  -H 'Content-Type: application/json' \
+#  -d '{ "message": "What is AnythingLLM?", "mode": "query" }'
+
+ curl -X 'POST' \
+  "${aHost}/${aAPI}" \
+  -H "accept: application/json" \
+  -H "Authorization: Bearer ${aANYLLM_API_KEY}" \
+  -H "Content-Type: application/json" \
+  -d "{ \"message\": \"${aPrompt}\", \"mode\": \"${aMode}\" }"
+
+exit
+
+aAUTH="Authorization: Bearer ${aANYLLM_API_KEY}"
+
+ aURL="'${aHost}/${aAPI}' -H 'accept: application/json' -H 'Authorization: Bearer ${aANYLLM_API_KEY}' -H 'Content-Type: application/json' -d '{ \"message\": \"${aPrompt}\", \"mode\": \"${aMode}\" }'"
+#'http://155.138.193.41:3001/api/v1/workspace/' -H 'accept: application/json' -H 'Authorization: Bearer ' -H 'Content-Type: application/json' -d '{ "message": "", "mode": "" }
+
+# echo "${aURL}"
+# echo "${aAUTH}"
+
+ curl -X 'POST' "${aURL}"
+
+
+
+
