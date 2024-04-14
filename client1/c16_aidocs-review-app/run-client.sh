1600689013a51e95fd6391bc40b34d2d0e0bc646
Robin Mattern <robin.mattern@gmail.com>
2024-04-13 20:51:16 -0400

    c40410.01_Improve PM2, run-client.sh scripts

---
+++ client1/c16_aidocs-review-app/run-client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash .
+
+if [ "${OSTYPE:0:5}" == "linux" ]; then
+   if which "run-node-apps"  >/dev/null 2>&1; then run-node-apps stop demo1; else echo -e "\n* run-node-apps is not available"; fi
+   if which "kill-node-apps" >/dev/null 2>&1; then kill-node-apps 8080 doit; else echo -e "\n* kill-node-apps is not available"; fi
+#  echo kill-node-apps 8080 doit
+#  kill-node-apps 8080 doit
+   npm start
+   fi
+
+if [ "${OSTYPE:0:4}" == "msys" ]; then
+   killport 8080
+   node server.mjs
+   fi
+
+if [ "${OS:0:7}" == "Windows" ]; then   # Running in DOS, will it ever happen?
+   killport 8080
+   node server.mjs
+   fi
+
+
+
