e6cfc660592d55d78c6e17879e3149b63f1dfe1b
Robin Mattern <robin.mattern@gmail.com>
2025-04-02 08:49:35 -0400

    .(50402.09_Add RepoDir .sh and PM2files

---
+++ renam'em.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function renameIt() {
+   aOld="$3"
+   aOld="${aOld/rm231d/rm228p}"
+   aNew="${aOld/demo1-master/dev01-robin}"
+
+   echo "mv ${aOld}  ---  ${aNew}"
+   mv "${aOld}" "${aNew}"
+     }
+
+   mv:              cannot move './._2/!2__Runtime Files for formR'\''s AIDocs Project in rm228p on Stage dev01-robin' 
+   to a subdirectory of itself, './._2/!2__Runtime Files for formR'\''s AIDocs Project in rm228p on Stage dev01-robin/!2__Runtime Files for formR'\''s AIDocs Project in rm228p on Stage dev01-robin'
+
+ renameIt  2025-03-06 15:27.58  "./client1/._3c1/TESTs/!3c1_Client1 Test Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client1/._3c1/!3c1__Client1 Runtime Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client1/._3c1/CSSs/!3c1_Client1 Stylesheets for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client1/._3c1/JSCs/!3c1_Client1 Javascript Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client1/._3c1/JSHs/!3c1_Client1 Shell Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client1/._3c1/NJSs/!3c1_Client1 NodeJS Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client1/!_Client1 Apps for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client1/c11_first-app/!_1st Client1 App for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./docs/!_Docsify Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/Cmds/!2_Windows Launch Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/FRTs/!2_formR Tools for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/JPTs/!2_JScriptWare Power Tools for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/JSHs/!2_JScriptWare Shell Scripts for formR's AIDocs Project in frm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/NJSs/!2_NodeJS Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/PS1s/!2_PowerShell Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/bin/!2_Shell Launch Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./._2/!2__Runtime Files for formR's AIDocs Project in rm231d on Stage dev01-robin"
+ renameIt  2025-03-06 15:27.58  "./client/!_Client Apps for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client/._3c0/!3c0__Client Runtime Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client/._3c0/CSSs/!3c0_Client Stylesheets for FormR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client/._3c0/JSCs/!3c0_Client Javascript Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client/._3c0/JSHs/!3c0_Client Shell Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client/._3c0/NJSs/!3c0_Client NodeJS Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:27.58  "./client/._3c0/TESTs/!3c0_Client Test Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server/!_Server APIs for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server/._3s0/!3s0__Server Runtime Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server/._3s0/CSSs/!3s0_Server Stylesheets for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server/._3s0/JSCs/!3s0_Server Javascript Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server/._3s0/JSHs/!3s0_Server Shell Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server/._3s0/NJSs/!3s0_Server NodeJS Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server/._3s0/TESTs/!3s0_Server Test Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/!_Server1 APIs for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/._3s1/!3s1__Server1 Runtime Files for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/._3s1/CSSs/!3s1_Server Stylesheets for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/._3s1/JSCs/!3s1_Server1 Javascript Files for FormR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/._3s1/JSHs/!3s1_Server1 Shell Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/._3s1/NJSs/!3s1_Server1 NodeJS Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/._3s1/TESTs/!3s1_Server1 Test Scripts for formR's AIDocs Project in rm231d on Stage demo1-master"
+ renameIt  2025-03-06 15:28.00  "./server1/s11_first-api/!_1st Server1 App for formR's AIDocs Project in rm231d on Stage demo1-master"
