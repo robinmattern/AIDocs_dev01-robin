0f6821a2d06ec7409c56679f3c9d7f775d474402
Robin Mattern <robin.mattern@gmail.com>
2025-04-05 13:49:33 -0400

    .(50405.03_Last commit of Stats file

--- server1/s12_websearch-app/testModels.sh
+++ server1/s12_websearch-app/testModels.sh
@@ -3,12 +3,14 @@
 # .env settings
 #-------------------------
 # SHOW_SCTIONS="RunId"    # "None,RunId,Stats,Parms,Docs,Search,Results"
-# SESSION_ID="t008"
+# SESSION_ID="t001"
 # NEXT_POST="01"
 # RUN_COUNT=2
 
-node interactive_search_u2.01.mjs  qwen2:0.5b         # Save docs/a12_*/a12_t007.01.4.50404.1101_Response.txt
-node interactive_search_u2.01.mjs  gemma2:2b          # Save docs/a12_*/a12_t007.03.4.50404.1102_Response.txt
-node interactive_search_u2.01.mjs  llama3      8000   # Save docs/a12_*/a12_t007.05.4.50404.1103_Response.txt
-node interactive_search_u2.01.mjs  llama3     16000   # Save docs/a12_*/a12_t007.07.4.50404.1104_Response.txt
+  node interactive_search_u2.02.mjs  qwen2:0.5b         # Save ./docs/a12_*/25.04.Apr/_t009_Final Test/a12_t009.01.4.50405.1143_Response.txt
+  node interactive_search_u2.02.mjs  gemma2:2b          # Save ./docs/a12_*/25.04.Apr/_t009_Final Test/a12_t009.02.4.50405.1143_Response.txt
+  node interactive_search_u2.02.mjs  llama3      8000   # Save ./docs/a12_*/25.04.Apr/_t009_Final Test/a12_t009.03.4.50405.1158_Response.txt
+  node interactive_search_u2.02.mjs  llama3.2   16000   # Save ./docs/a12_*/25.04.Apr/_t009_Final Test/a12_t009.04.4.50405.1158_Response.txt
+
+
 
